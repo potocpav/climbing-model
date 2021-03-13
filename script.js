781b7a393dff8fcc387ed8657b40eef68afea4ad
Pavel Potocek <pavelpotocek@gmail.com>
2021-03-13 18:05:05 +0100

    Make everything const

--- script.js
+++ script.js
@@ -2,76 +2,76 @@
 
 // Maths
 
-let l2 = ([ax, ay], [bx, by]) => {
+const l2 = ([ax, ay], [bx, by]) => {
   return Math.sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
 }
 
-let project = (camera, w, h) => pt => {
-  let scale = w / camera.width;
+const project = (camera, w, h) => pt => {
+  const scale = w / camera.width;
   return [(pt[0] - camera.center[0]) * scale + w / 2, h / 2 - (pt[1] - camera.center[1]) * scale];
 };
 
-let project_dir = (camera, w) => dir => {
-  let scale = w / camera.width;
+const project_dir = (camera, w) => dir => {
+  const scale = w / camera.width;
   return [dir[0] * scale, -dir[1] * scale];
 };
 
-let inv_project = (camera, w, h) => pt => {
-  let scale = camera.width / w;
+const inv_project = (camera, w, h) => pt => {
+  const scale = camera.width / w;
   return [(pt[0] - w / 2) * scale + camera.center[0], camera.center[1] - (pt[1] - h / 2) * scale];
 };
 
-let interpolate = (p1, p2, t) => {
+const interpolate = (p1, p2, t) => {
   return [p1[0] * (1 - t) + p2[0] * t, p1[1] * (1 - t) + p2[1] * t];
 }
 
-let norm = ([px, py]) => {
+const norm = ([px, py]) => {
   return Math.sqrt(px * px + py * py)
 };
 
 /// See `maths.py` for the derivation of the equations below.
-let calc_forces = climber => {
-  let r_1x = climber.hands[0] - climber.center[0];
-  let r_1y = climber.hands[1] - climber.center[1];
-  let r_2x = climber.feet[0] - climber.center[0];
-  let r_2y = climber.feet[1] - climber.center[1];
-  let g = 1;
+const calc_forces = climber => {
+  const r_1x = climber.hands[0] - climber.center[0];
+  const r_1y = climber.hands[1] - climber.center[1];
+  const r_2x = climber.feet[0] - climber.center[0];
+  const r_2y = climber.feet[1] - climber.center[1];
+  const g = 1;
 
-  let fac = 1 / (r_1x * r_2y - r_1y * r_2x);
-  let dist = Math.pow(r_1x - r_2x, 2) + Math.pow(r_1y - r_2y, 2);
+  const fac = 1 / (r_1x * r_2y - r_1y * r_2x);
+  const dist = Math.pow(r_1x - r_2x, 2) + Math.pow(r_1y - r_2y, 2);
 
   // Forces, depending on torque `t`
-  let f1 = t => [
+  const f1 = t => [
     fac * (g * r_1x * r_2x - t * (r_1x - r_2x)),
     fac * (g * r_1y * r_2x - t * (r_1y - r_2y))];
 
-  let f2 = t => [
+  const f2 = t => [
     fac * (-g * r_1x * r_2x + t * (r_1x - r_2x)),
     fac * (-g * r_1x * r_2y + t * (r_1y - r_2y))];
 
   // Minimum force parameters
-  let min_hands_t = g * r_2x / dist *
+  const min_hands_t = g * r_2x / dist *
     (r_1x * (r_1x - r_2x) + r_1y * (r_1y - r_2y));
-  let min_feet_t = g * r_1x / dist *
+  const min_feet_t = g * r_1x / dist *
     (r_2x * (r_1x - r_2x) + r_2y * (r_1y - r_2y));
 
     return {f1, f2, min_hands_t, min_feet_t};
 };
 
-let calc_torque = ([r_x, r_y], [f_x, f_y]) => {
+const calc_torque = ([r_x, r_y], [f_x, f_y]) => {
   return Math.abs(r_x * f_y - r_y * f_x);
 }
 
-let minus = ([ax, ay], [bx, by]) => [ax - bx, ay - by];
+const minus = ([ax, ay], [bx, by]) => [ax - bx, ay - by];
 
-let plus = ([ax, ay], [bx, by]) => [ax + bx, ay + by];
+const plus = ([ax, ay], [bx, by]) => [ax + bx, ay + by];
 
-let dot = ([ax, ay], [bx, by]) => ax * bx + ay * by;
+const dot = ([ax, ay], [bx, by]) => ax * bx + ay * by;
 
 // Draw
 
-let draw_force_range = (ctx, pos, f1, f2, color) => {
-  let scale = 1.0;
+const draw_force_range = (ctx, pos, f1, f2, color) => {
+  const scale = 1.0;
 
   ctx.beginPath();
   ctx.globalAlpha = 0.2
@@ -82,15 +82,15 @@ let draw_force_range = (ctx, pos, f1, f2, color) => {
   ctx.fill();
 };
 
-let draw_force = (ctx, pos, f, color) => {
-  let scale = 1.0;
+const draw_force = (ctx, pos, f, color) => {
+  const scale = 1.0;
 
   // force arrow head computation
-  let width = 3;
-  let head_size = width * 2;
-  let dx = f[0] / norm(f);
-  let dy = f[1] / norm(f);
-  let tip = [pos[0] + f[0] * scale, pos[1] + f[1] * scale];
+  const width = 3;
+  const head_size = width * 2;
+  const dx = f[0] / norm(f);
+  const dy = f[1] / norm(f);
+  const tip = [pos[0] + f[0] * scale, pos[1] + f[1] * scale];
 
   // draw the arrow
   ctx.beginPath();
@@ -105,8 +105,8 @@ let draw_force = (ctx, pos, f, color) => {
   ctx.stroke();
 };
 
-let draw = (canvas, image, state) => {
-  let draw_circle = ([x, y], r, color) => {
+const draw = (canvas, image, state) => {
+  const draw_circle = ([x, y], r, color) => {
     ctx.beginPath();
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
@@ -119,29 +119,29 @@ let draw = (canvas, image, state) => {
 
   var ctx = canvas.getContext("2d");
 
-  let project_ap = project(state.camera, canvas.width, canvas.height);
-  let project_dir_ap = project_dir(state.camera, canvas.width);
+  const project_ap = project(state.camera, canvas.width, canvas.height);
+  const project_dir_ap = project_dir(state.camera, canvas.width);
 
-  let center_px = project_ap(state.climber.center);
-  let feet_px = project_ap(state.climber.feet);
-  let hands_px = project_ap(state.climber.hands);
+  const center_px = project_ap(state.climber.center);
+  const feet_px = project_ap(state.climber.feet);
+  const hands_px = project_ap(state.climber.hands);
 
   ctx.lineWidth = 3;
 
-  let [bg_w, bg_h] = [image.naturalWidth, image.naturalHeight];
+  const [bg_w, bg_h] = [image.naturalWidth, image.naturalHeight];
   if (bg_w == 0) {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   } else {
-    let s = Math.min(bg_w, bg_h);
-    let [mx, my] = [(bg_w - s) / 2, (bg_h - s) / 2];
+    const s = Math.min(bg_w, bg_h);
+    const [mx, my] = [(bg_w - s) / 2, (bg_h - s) / 2];
     ctx.drawImage(image, mx, my, s, s, 0, 0, canvas.width, canvas.height);
   }
 
   // Draw the wall
   ctx.globalAlpha = 0.7;
   let [dx, dy] = minus(hands_px, feet_px);
-  let invert = dot([dy, -dx], minus(center_px, feet_px)) > 0 ? 1 : -1;
+  const invert = dot([dy, -dx], minus(center_px, feet_px)) > 0 ? 1 : -1;
   [dx, dy] = [dx / norm([dx, dy]) * invert, dy / norm([dx, dy]) * invert];
 
   var my_gradient = ctx.createLinearGradient(feet_px[0], feet_px[1], feet_px[0] - dy, feet_px[1] + dx);
@@ -164,10 +164,10 @@ let draw = (canvas, image, state) => {
   draw_circle(hands_px, 10, "green");
 
   // Draw forces
-  let forces = calc_forces(state.climber);
-  let t = forces.min_hands_t * (1 - state.force) + forces.min_feet_t * state.force;
-  let f_hands = forces.f1(t);
-  let f_feet = forces.f2(t);
+  const forces = calc_forces(state.climber);
+  const t = forces.min_hands_t * (1 - state.force) + forces.min_feet_t * state.force;
+  const f_hands = forces.f1(t);
+  const f_feet = forces.f2(t);
 
   draw_force_range(ctx, hands_px,
     project_dir_ap(forces.f1(forces.min_hands_t)),
@@ -197,24 +197,24 @@ let draw = (canvas, image, state) => {
 
 };
 
-let update_ui = state => {
-  let canvas = document.getElementById("canvas");
-  let slider = document.getElementById("force_slider");
-  let bg_img = document.getElementById("bg_image");
+const update_ui = state => {
+  const canvas = document.getElementById("canvas");
+  const slider = document.getElementById("force_slider");
+  const bg_img = document.getElementById("bg_image");
   draw(canvas, bg_img, state);
   slider.value = Math.round(state.force * 100);
 };
 
-let default_state = () => {
-  let climber = {
+const default_state = () => {
+  const climber = {
     center: [-0.5, -0.5],
     feet: [1, -1],
     hands: [-0.5, 1]
   };
 
-  let camera = {"center": [0, 0], "width": 5};
+  const camera = {"center": [0, 0], "width": 5};
 
-  let state = {
+  const state = {
     climber: climber,
     camera: camera,
     dragging_center: false,
@@ -226,15 +226,15 @@ let default_state = () => {
   return state;
 }
 
-let serialize = state => {
-  let [c, f] = [state.climber, state.force];
-  let json = [c.center[0], c.center[1], c.feet[0], c.feet[1], c.hands[0], c.hands[1], f]
+const serialize = state => {
+  const [c, f] = [state.climber, state.force];
+  const json = [c.center[0], c.center[1], c.feet[0], c.feet[1], c.hands[0], c.hands[1], f]
     .map(i => Math.round(i * 100) / 100);
   return JSON.stringify(json);
 };
 
-let from_list = ([a, b, c, d, e, f, g]) => {
-  let state = default_state();
+const from_list = ([a, b, c, d, e, f, g]) => {
+  const state = default_state();
   state.climber.center = [a, b];
   state.climber.feet = [c, d];
   state.climber.hands = [e, f];
@@ -242,16 +242,16 @@ let from_list = ([a, b, c, d, e, f, g]) => {
   return state;
 }
 
-let deserialize = s => {
-  let state = default_state();
-  let l = JSON.parse(s).map(s => Number(s));
+const deserialize = s => {
+  const state = default_state();
+  const l = JSON.parse(s).map(s => Number(s));
   return from_list(l);
 };
 
 let state = default_state();
 
 // This function is global, so that it can be used in HTML links directly
-let set_state = l => {
+const set_state = l => {
   state = from_list(l);
   update_ui(state);
 }
@@ -259,15 +259,15 @@ let set_state = l => {
 window.onload = (event) => {
   // Events
 
-  let canvas = document.getElementById("canvas");
-  let slider = document.getElementById("force_slider");
-  let bg_image = document.getElementById("bg_image");
+  const canvas = document.getElementById("canvas");
+  const slider = document.getElementById("force_slider");
+  const bg_image = document.getElementById("bg_image");
 
 
   let mouseIsDown = false;
 
-  let mouseDown = (state, x, y) => {
-    let project_ap = project(state.camera, canvas.width, canvas.height);
+  const mouseDown = (state, x, y) => {
+    const project_ap = project(state.camera, canvas.width, canvas.height);
 
     if (l2([x, y], project_ap(state.climber.center)) < 20) {
       state.dragging_center = true;
@@ -283,9 +283,9 @@ window.onload = (event) => {
     return state;
   };
 
-  let mouseDrag = (state, x, y) => {
-    let inv_project_ap = inv_project(state.camera, canvas.width, canvas.height);
-    let new_pos = inv_project_ap([x, y]);
+  const mouseDrag = (state, x, y) => {
+    const inv_project_ap = inv_project(state.camera, canvas.width, canvas.height);
+    const new_pos = inv_project_ap([x, y]);
 
     if (state.dragging_center) {
       state.climber.center = new_pos;
@@ -301,7 +301,7 @@ window.onload = (event) => {
     return state;
   }
 
-  let mouseUp = (state) => {
+  const mouseUp = (state) => {
     state.dragging_center = false;
     state.dragging_feet = false;
     state.dragging_hands = false;
@@ -372,7 +372,7 @@ window.onload = (event) => {
   };
 
   window.onresize = () => {
-    let max_width = window.innerHeight * 0.8;
+    const max_width = window.innerHeight * 0.8;
     canvas.width = Math.min(max_width, slider.getBoundingClientRect().width);
     canvas.height = canvas.width;
     update_ui(state);
